bf2d08149d1d7a3a736a16c2cc89e5e1e9b6f280
Christian Halsell <christian.halsell@verys.com>
2019-01-10 15:52:59 -0800

    refactored stateless components

--- src/components/CurrentConditions.js
+++ src/components/CurrentConditions.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const CurrentConditions = (props) => {
-  return (
-    <div>
-      <h2>{props.currentTemperature}&deg;</h2>
-      <p>{props.currentWeather} <img src={props.currentIconSrc} /></p>        
-    </div>
-  );
-};
+const CurrentConditions = (props) => (
+  <div>
+    <h2>{props.currentTemperature}&deg;</h2>
+    <p>{props.currentWeather} <img src={props.currentIconSrc} /></p>        
+  </div>
+);
 
 export default CurrentConditions;
--- src/components/ForecastDay.js
+++ src/components/ForecastDay.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const ForecastDay = (props) => {
-  return (
-    <div>
-      {props.day} {props.conditions} <img src={props.imgsrc} width="32" height="32" /> High: {props.high} Low: {props.low}
-    </div>
-  );
-};
+const ForecastDay = (props) => (
+  <div>
+    {props.day} {props.conditions} <img src={props.imgsrc} width="32" height="32" /> High: {props.high} Low: {props.low}
+  </div>
+);
 
 export default ForecastDay;
