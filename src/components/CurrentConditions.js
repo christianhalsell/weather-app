eaa542e82d6ed557f0aa098723c7b1cd2e28d337
Christian Halsell <christian.halsell@verys.com>
2019-01-17 10:23:13 -0800

    added items to current conditions and favicon

--- src/components/CurrentConditions.js
+++ src/components/CurrentConditions.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
 const CurrentConditions = (props) => (
   <div>
     <h2>{props.currentTemperature}&deg;</h2>
-    <p>{props.currentWeather} <img src={props.currentIconSrc} /></p>        
+    <p><img src={props.currentIconSrc} /></p>
+    <p>{props.currentWeather}</p>
+    <p>{moment(props.currentDate).format('ddd, MMMM Do YYYY')}</p>   
+    <p>{props.currentHigh}&deg;/{props.currentLow}&deg;</p> 
   </div>
 );
 
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -10,20 +10,25 @@ const Forecast = (props) => (
         currentTemperature={props.currentConditions.temp_f}
         currentIconSrc={props.currentConditions.condition.icon}
         currentWeather={props.currentConditions.condition.text}
+        currentDate={props.forecast[0].date}
+        currentHigh={props.forecast[0].day.maxtemp_f}
+        currentLow={props.forecast[0].day.mintemp_f}
       />
     }
     {
-      props.forecast.map((day) => {
-        return (
-          <ForecastDay 
-            key={day.date}
-            date={day.date} 
-            conditions={day.day.condition.text} 
-            imgsrc={day.day.condition.icon}
-            high={day.day.maxtemp_f}
-            low={day.day.mintemp_f}
-          />
-        )
+      props.forecast.map((day, index) => {
+        if (index !== 0) {
+          return (
+            <ForecastDay 
+              key={day.date}
+              date={day.date} 
+              conditions={day.day.condition.text} 
+              imgsrc={day.day.condition.icon}
+              high={day.day.maxtemp_f}
+              low={day.day.mintemp_f}
+            />
+          )
+        }        
       })
     }
   </div>      
