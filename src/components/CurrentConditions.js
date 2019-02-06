d8d3f510a229a9c9e7ff36d153c97c087272e3dd
Christian Halsell <christian.halsell@verys.com>
2019-02-06 11:54:39 -0800

    current forecast layout

--- src/components/CurrentConditions.js
+++ src/components/CurrentConditions.js
@@ -4,9 +4,13 @@ import moment from 'moment';
 const CurrentConditions = (props) => (
   <div className="current-conditions">
     <div className="current-conditions__summary">
-      <div><img src={props.currentIconSrc} /></div>
-      <div>{props.currentWeather}</div>
-      <div>{moment(props.currentDate).format('ddd, MMMM Do YYYY')}</div>
+      <div>
+        <img src={props.currentIconSrc} />
+      </div>
+      <div>
+        <div className="current-conditions__header">{props.currentWeather}</div>
+        <div className="current-conditions__date">{moment(props.currentDate).format('ddd, MMMM Do YYYY')}</div>
+      </div>      
     </div>  
     
     <div className="current-conditions__details">
