f152c13b83c61b5d72f6945dba05f6a0ab6c4790
Christian Halsell <christian.halsell@verys.com>
2019-02-08 14:03:13 -0800

    error message relocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,10 @@ const Header = (props) => {
   return (
     <div className="header-wrapper">
       <div className="header">
-        <div className="header__title">{title}</div>
+        <div className="header__title">
+          {title}
+          { props.error && <div>{props.error}</div> }
+        </div>        
         <div className="header__desktop-form">
           <InputForm getWeather={props.getWeather} />
         </div>
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -66,8 +66,7 @@ export default class WeatherApp extends React.Component {
     
     return (
       <div id="weatherApp" className="weather-app">
-        <Header location={location} openInputModal={this.openInputModal} getWeather={this.getWeather} />
-        { error && <div>{error}</div> }
+        <Header error={error} location={location} openInputModal={this.openInputModal} getWeather={this.getWeather} />
         { forecast && <Forecast currentConditions={currentConditions} forecast={forecast} /> }
         <InputModal
           inputModal={this.state.inputModal}
